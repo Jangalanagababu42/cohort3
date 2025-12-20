0bb646ea57ffc5f2853a444d046d535a0a627cf0
Jangalanagababu42 <60348426+Jangalanagababu42@users.noreply.github.com>
2025-12-20 14:33:48 +0530

    wrap up to github

---
+++ web2/index.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    // learn typecasting in cpp
+    
+    // int a = 1;
+    
+    // a = true;
+    bool a =true;
+    a = "harkirat";
+    std::cout << a;
+}
