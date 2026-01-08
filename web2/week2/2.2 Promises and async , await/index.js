be8fd07698a6f90b79b78024289f353f94e3aa8a
Jangalanagababu42 <60348426+Jangalanagababu42@users.noreply.github.com>
2026-01-08 21:23:28 +0530

    classes example

---
+++ web2/week2/2.2 Promises and async , await/index.js
@@ -0,0 +1,20 @@
+class Rectangle {
+  // class
+  constructor(width, height, color) {
+    //constructor
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+  area() {
+    //methods
+    return this.width * this.height;
+  }
+  paint() {
+    console.log("the colour of Rectangle", this.color);
+  }
+}
+
+const rect = new Rectangle(3, 77, "green"); //obj
+console.log(rect.area()); // calling obj method
+rect.paint();
